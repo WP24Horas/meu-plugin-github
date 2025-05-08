bbee94c3f84b6fa7a1911a33643c7c76855a3d1d
Asllan Maciel <asllanmaciel@gmail.com>
2025-05-08 09:11:27 -0300

    Atualiza a versão do plugin para 1.0.3 e ajusta a lógica de verificação de atualizações

--- includes/GitHubUpdater.php
+++ includes/GitHubUpdater.php
@@ -4,47 +4,60 @@ class GitHubUpdater
 {
     private $repo_url = 'https://api.github.com/repos/WP24Horas/meu-plugin-github';
     private $zip_url = 'https://github.com/WP24Horas/meu-plugin-github/releases/download/1.0.2/meu-plugin-github.zip';
-    private $version = '1.0.2'; // versão atual do plugin local
+    private $version;
 
     private $plugin_file;
 
     public function __construct()
-    {
-        $this->plugin_file = plugin_basename(MPG_PLUGIN_FILE);
-        $this->version = get_plugin_data(MPG_PLUGIN_FILE)['Version'];
-
-        add_filter('pre_set_site_transient_update_plugins', [$this, 'check_update']);
-        add_filter('plugins_api', [$this, 'plugin_info'], 10, 3);
+{
+    if ( ! function_exists( 'get_plugin_data' ) ) {
+        require_once ABSPATH . 'wp-admin/includes/plugin.php';
     }
+
+    $plugin_data = get_plugin_data(MPG_PLUGIN_FILE);
+    $this->version = $plugin_data['Version'];
+
+    $this->plugin_file = plugin_basename(MPG_PLUGIN_FILE);
+
+    add_filter('pre_set_site_transient_update_plugins', [$this, 'check_update']);
+    add_filter('plugins_api', [$this, 'plugin_info'], 10, 3);
+}
+
 
     public function check_update($transient)
-    {
-        if (empty($transient->checked)) {
-            return $transient;
-        }
-
-        $response = wp_remote_get("{$this->repo_url}/releases/latest", [
-            'headers' => ['Accept' => 'application/vnd.github.v3+json'],
-        ]);
-
-        if (is_wp_error($response)) {
-            return $transient;
-        }
-
-        $release = json_decode(wp_remote_retrieve_body($response));
-
-        if (version_compare($this->version, $release->tag_name, '<')) {
-            $transient->response[$this->plugin_file] = (object)[
-                'slug'        => dirname($this->plugin_file),
-                'plugin'      => $this->plugin_file,
-                'new_version' => $release->tag_name,
-                'package'     => $this->zip_url,
-                'url'         => $release->html_url,
-            ];
-        }
-
+{
+    if (empty($transient->checked)) {
         return $transient;
     }
+
+    $response = wp_remote_get("{$this->repo_url}/releases/latest", [
+        'headers' => [
+            'Accept' => 'application/vnd.github.v3+json',
+            'User-Agent' => 'WordPress/' . get_bloginfo('version') . '; ' . home_url(),
+        ],
+    ]);
+
+    if (is_wp_error($response)) {
+        return $transient;
+    }
+
+    $release = json_decode(wp_remote_retrieve_body($response));
+
+    $latest = ltrim($release->tag_name, 'v');
+
+    if (version_compare($this->version, $latest, '<')) {
+        $transient->response[$this->plugin_file] = (object)[
+            'slug'        => dirname($this->plugin_file),
+            'plugin'      => $this->plugin_file,
+            'new_version' => $latest,
+            'package'     => "https://github.com/WP24Horas/meu-plugin-github/releases/download/{$release->tag_name}/meu-plugin-github.zip",
+            'url'         => $release->html_url,
+        ];
+    }
+
+    return $transient;
+}
+
 
     public function plugin_info($res, $action, $args)
     {
